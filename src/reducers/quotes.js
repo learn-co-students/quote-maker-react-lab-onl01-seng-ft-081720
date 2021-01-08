e006adb175394616191ff469fed834d76ce56049
atBeatty <adam.t.beatt@gmail.com>
2021-01-07 21:12:06 -0800

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,31 @@
+// import uuid from 'uuid'
+
 export default (state = [], action) => {
-  return state;
+  switch(action.type) {
+    case "ADD_QUOTE":
+      console.log(state)
+      return [...state, action.quote];
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId)
+    case "UPVOTE_QUOTE":
+        let targetQuote = state.find(q => q.id === action.quoteId)
+        targetQuote.votes += 1
+      return [{...state[0], votes: targetQuote.votes}]
+
+    case "DOWNVOTE_QUOTE":
+      let votedDownQuote = state.find(q => q.id === action.quoteId)
+        if (votedDownQuote.votes && votedDownQuote.votes > 0) {
+          votedDownQuote = {...votedDownQuote, votes: votedDownQuote.votes -=1}
+          
+
+        // } else {
+        //   votedDownQuote = {...votedDownQuote, votes: 0}
+        }
+        console.log(votedDownQuote)
+      return state
+
+    default:
+      return state;
+  }
+  
 }
